9052f08107c679c328b52d5060a687fd6366368f
Nita Gabriel <62211186+gabyiy@users.noreply.github.com>
2022-03-20 03:01:42 +0100

    next

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const app= express()
 
 app.set("view engine", "ejs")
 
-app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 mongoose.connect("mongodb+srv://gabi:711334@cluster0.4uo4k.mongodb.net/portfolioDB?retryWrites=true&w=majority")
 
